9e9d02fa4346731f4aa12cb89227c28cdf89d820
Abed Islam <aislam360@gmail.com>
2020-05-05 06:55:04 -0400

    routes setup. need to add buttons

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import './App.css';
 
 import PartyList from './components/PartyList'
 import BattlePage from './components/BattlePage'
+import PartyForm from './components/PartyForm'
+import UsernameForm from './components/UsernameForm'
 
 function App() {
 
@@ -21,6 +23,8 @@ function App() {
   
             <Router>
                 <Switch>
+                    <Route path="/UsernameForm" component={UsernameForm}/>
+                    <Route path="/PartyForm" component={PartyForm}/>
                     <div className="App" style={ containerStyles } >
                         <Route exact path="/abislam/pokemon-frontend" component={PartyList} />
                         <Route path="/battlepage" component={BattlePage} />
